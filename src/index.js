f16819815f7905448e6ef4cafb4bfd403d5f5c9d
Ilya Pridanov <pridanov.ilya@mail.ru>
2020-10-25 18:58:50 +0400

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let flag;
+    if (str.length % 2 !== 0) {
+        flag = false;
+    }
+
+    const map = new Map(bracketsConfig);
+    let stack = [];
+    const arr = str.split('');
+    arr.forEach((el, i) => {
+        if (map.has(el) && !(el === stack[stack.length - 1] && el === map.get(el))) {
+            stack.push(el);
+        } else if (el === map.get(stack[stack.length - 1])) {
+            stack.pop();
+        }
+    });
+
+    if (stack.length === 0 && str.length % 2 === 0) {
+        flag = true;
+    } else {
+        flag = false;
+    }
+    console.log(flag, stack);
+    return flag;
 }
